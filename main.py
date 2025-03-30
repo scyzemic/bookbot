4ff18b2f7b3ce109672af1d14482f7ca3712ef5f
scyzemic <11906352+scyzemic@users.noreply.github.com>
2025-03-30 19:38:14 -0400

    feat: read a file and print it to stdout

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+
+def get_book_text(file_path: str):
+    return open(file_path).read()
+
+def main():
+    book_text = get_book_text('./books/frankenstein.txt')
+    print(book_text)
+
+main()
