f8af59fc23677545e4e067ebd572220af535ff1a
scyzemic <11906352+scyzemic@users.noreply.github.com>
2025-03-30 19:55:49 -0400

    feat: count characters

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from stats import get_word_count, get_character_counts
 
-from stats import get_word_count
 
 def get_book_text(file_path: str):
     return open(file_path).read()
 
+
 def main():
-    book_text = get_book_text('./books/frankenstein.txt')
+    book_text = get_book_text("./books/frankenstein.txt")
     word_count = get_word_count(book_text)
+    characters = get_character_counts(book_text)
     print(f"{word_count} words found in the document")
+    print(characters)
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
-
 def get_word_count(text: str):
     return len(text.split())
+
+
+def get_character_counts(text: str):
+    characters: dict[str, int] = {}
+
+    for char in text.lower():
+        characters[char] = characters.get(char, 0) + 1
+
+    return characters
