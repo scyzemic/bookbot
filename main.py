6fb6886ce21af43a93eb821d48b988f8b263ed73
scyzemic <11906352+scyzemic@users.noreply.github.com>
2025-03-30 19:40:52 -0400

    feat: add word count

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 def get_book_text(file_path: str):
     return open(file_path).read()
 
+def get_word_count(text: str):
+    return len(text.split())
+
 def main():
     book_text = get_book_text('./books/frankenstein.txt')
-    print(book_text)
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words found in the document")
 
 main()
