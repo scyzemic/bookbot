3470003fd5bc5d623887cd485b6ce799ef2fc18e
scyzemic <11906352+scyzemic@users.noreply.github.com>
2025-03-30 20:49:18 -0400

    feat: print full report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_character_counts
+from stats import get_word_count, get_character_counts, format_character_counts
 
 
 def get_book_text(file_path: str):
@@ -9,8 +9,19 @@ def main():
     book_text = get_book_text("./books/frankenstein.txt")
     word_count = get_word_count(book_text)
     characters = get_character_counts(book_text)
-    print(f"{word_count} words found in the document")
-    print(characters)
+    sorted_characters = format_character_counts(characters)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for character in sorted_characters:
+        if str(character["char"]).isalpha():
+            print(f"{character['char']}: {character['count']}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,15 @@ def get_character_counts(text: str):
         characters[char] = characters.get(char, 0) + 1
 
     return characters
+
+
+def format_character_counts(characters: dict[str, int]):
+    sorted_chars: list[dict[str, str | int]] = []
+
+    for char, count in characters.items():
+        char_dict = {"char": char, "count": count}
+        sorted_chars.append(char_dict)
+
+    sorted_chars.sort(key=lambda x: x["count"], reverse=True)
+
+    return sorted_chars
